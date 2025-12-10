aadcce6b632cf110454bc73b24868f880d0d463c
Aleksandra Tsishkouskaya <alesandra.arti@gmail.com>
2025-12-10 12:17:43 +0100

    add variables, alert, and console.log to lesson01

---
+++ script.js
@@ -0,0 +1,9 @@
+let title = "JS Task Title";
+let screens = 5;
+let screenPrice = 100;
+rollback = 10;
+fullPrice = screens * screenPrice;
+adaptive = true;
+
+alert("Hello!");
+console.log("Task Title:", title);
