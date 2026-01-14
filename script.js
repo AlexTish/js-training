6cd5ef9f04313c85c7084bba3413acedea998573
Aleksandra Tsishkouskaya <alesandra.arti@gmail.com>
2026-01-14 12:02:23 +0100

    add: assigning values ​​to variables, using methods and properties

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 let title = "JS Task Title";
-let screens = 5;
-let screenPrice = 100;
+let screens = "Простые, Сложные, Интерактивные";
+let screenPrice = 1000;
 let rollback = 10;
-let fullPrice = screens * screenPrice;
+let fullPrice = 1000000;
 let adaptive = true;
 
-alert("Hello!");
-console.log("Task Title:", title);
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+console.log(screens.length);
+console.log(`Стоимость верстки экранов ${screenPrice} рублей`);
+console.log(`Стоимость разработки сайта ${fullPrice} рублей`);
+const screenArray = screens.toLowerCase().split(", ");
+console.log("Массив:", screenArray);
+const rollbackAmount = fullPrice * (rollback/100);
+console.log("Процент отката посреднику за работу:", rollbackAmount);
